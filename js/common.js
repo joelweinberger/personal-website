de243ae015615c27778442d0cb5214b5e45153ae
jww <jww@4083d6ec-3a80-4b62-8457-a62792b4a8b0>
2009-06-30 00:27:38 +0000

    made abstract icon position better
    git-svn-id: file:///share/website/berkeley-cs2@21 4083d6ec-3a80-4b62-8457-a62792b4a8b0

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 var toggle = (function () {
     function hide(id) {
         var elmt = $(id);
-        var details = $(id).select('.details')[0];
+        var details = $(id).select('.paper-details')[0];
         elmt.removeClassName('open');
         elmt.addClassName('closed');
         details.slideUp({duration: 0.3, queue: 'end'});
@@ -9,14 +9,14 @@ var toggle = (function () {
     
     function show(id) {
         var elmt = $(id);
-        var details = $(id).select('.details')[0];
+        var details = $(id).select('.paper-details')[0];
         elmt.removeClassName('closed');
         elmt.addClassName('open');
         details.slideDown({duration: 0.3, queue: 'end'});
     }
     
     return function (id) {
-        if ($(id).select('.details')[0].visible())
+        if ($(id).select('.paper-details')[0].visible())
             hide(id);
         else
             show(id);
