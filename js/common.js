1ec87b9c88696cd01addcb99359a1484c1babad3
jww <jww@4083d6ec-3a80-4b62-8457-a62792b4a8b0>
2009-07-24 19:11:25 +0000

    switch to mootools
    git-svn-id: file:///share/website/berkeley-cs2@40 4083d6ec-3a80-4b62-8457-a62792b4a8b0

--- js/common.js
+++ js/common.js
@@ -1,24 +1,54 @@
-var toggleById = (function () {
-    function hide(id) {
-        var elmt = $(id);
-        var details = $(id).select('.paper-details')[0];
-        elmt.removeClassName('open');
-        elmt.addClassName('closed');
-        details.slideUp({duration: 0.3, queue: 'end'});
+var toggleById = function (id) {
+    var paper = $(id);
+    var abst = $(id + '-abstract');
+
+    if (abst.hasClass('start-hidden')) {
+        abst.removeClass('start-hidden');
+        abst.slide('hide');
     }
-    
-    function show(id) {
-        var elmt = $(id);
-        var details = $(id).select('.paper-details')[0];
-        elmt.removeClassName('closed');
-        elmt.addClassName('open');
-        details.slideDown({duration: 0.3, queue: 'end'});
+
+    abst.slide('toggle');
+
+    if (abst.isOpen()) {
+        paper.addClass('open');
+        paper.removeClass('closed');
+    } else {
+        paper.addClass('closed');
+        paper.removeClass('open');
     }
-    
-    return function (id) {
-        if ($(id).select('.paper-details')[0].visible())
-            hide(id);
-        else
-            show(id);
+};
+
+function attachToggle(name, contents) {
+    var paper = $(name);
+    var elmt = $(name + '-toggle');
+
+    // add anchor at runtime in JS so that users without JavaScript don't see
+    // the anchor, but it's still there for those who want to use the keyboard
+    paper.addClass('start-hidden');
+    elmt.innerHTML = '<a href="javascript:" class="toggle">' + contents + '</a>';
+    elmt.getElementsByTagName('a')[0].onclick = collapsibleOnClick(name);
+}
+
+function collapsibleOnClick(name) {
+    var paper = $(name);
+    var slidefx;
+
+    return function() {
+        if (!$defined(slidefx)) {
+            slidefx = new Fx.Slide(name + '-abstract');
+            paper.removeClass('start-hidden');
+            slidefx.hide();
+        }
+
+        //slidefx.stop();
+        if(paper.hasClass('closed')) {
+            slidefx.slideIn();
+            paper.removeClass('closed');
+            paper.addClass('open');
+        } else {
+            slidefx.slideOut();
+            paper.removeClass('open');
+            paper.addClass('closed');
+        }
     };
-})();
+}
