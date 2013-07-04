fedfc0b54373b1d85a8afd3fd812b91db7bb8d3e
Joel Howard Willis Weinberger <joel.weinberger@gmail.com>
2013-07-04 16:22:01 -0700

    Added basic node files.

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express')
+  , hbs = require('express-hbs')
+  , http = require('http')
+  , path = require('path')
+  , routes = require('./routes');
+
+var app = express();
+
+// Hook in express-hbs and tell it where known directories reside
+app.engine('hbs', hbs.express3({
+    partialsDir: __dirname + '/views/',
+    defaultLayout: __dirname + '/views/layout.hbs'
+}));
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'hbs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/index', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
