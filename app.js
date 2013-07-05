24f6e77c0bc732be14446ade288464d349ddaa93
Joel Howard Willis Weinberger <joel.weinberger@gmail.com>
2013-07-04 19:07:02 -0700

    Add extra scripts and css dynamically

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/index', routes.index);
+app.get('/calendar', routes.calendar);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,17 @@
 exports.index = function(req, res) {
-  res.render('index');
+  res.render('index', {
+    title: 'jww',
+    extracss: [
+      'css/page/index.css'
+    ],
+    extrascripts: [
+      'js/index.js',
+      'lib/mootools.js',
+      'lib/mootools-more.js'
+    ]
+  });
+};
+
+exports.calendar = function(req, res) {
+  res.render('calendar');
 };
